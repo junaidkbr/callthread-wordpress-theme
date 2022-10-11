23f531ff9c69f69483bc6f0db53a260678286b04
Junaid Ahmed <junaidkbr@gmail.com>
2022-10-11 23:07:55 +0500

    Removes unused code

--- functions.php
+++ functions.php
@@ -21,17 +21,6 @@ if ( ! defined( '_S_VERSION' ) ) {
  */
 function create_ape_theme_setup() {
 	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on create-ape-theme, use a find and replace
-		* to change 'create-ape-theme' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'create-ape-theme', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
 		* Let WordPress manage the document title.
 		* By adding theme support, we declare that this theme does not use a
 		* hard-coded <title> tag in the document head, and expect WordPress to
@@ -69,70 +58,8 @@ function create_ape_theme_setup() {
 			'script',
 		)
 	);
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'create_ape_theme_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
 }
 add_action( 'after_setup_theme', 'create_ape_theme_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function create_ape_theme_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'create_ape_theme_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'create_ape_theme_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function create_ape_theme_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'create-ape-theme' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'create-ape-theme' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'create_ape_theme_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -142,10 +69,6 @@ function create_ape_theme_scripts() {
 	wp_style_add_data( 'create-ape-theme-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'create-ape-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'create_ape_theme_scripts' );
 
@@ -181,7 +104,7 @@ require get_template_directory() . '/inc/template-functions.php';
  *
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function ca_register_required_plugins() {
+function create_ape_register_required_plugins() {
 	/*
 	 * Array of plugin arrays. Required keys are name and slug.
 	 * If the source is NOT from the .org repo, then source is also required.
@@ -211,7 +134,7 @@ function ca_register_required_plugins() {
 
 	tgmpa( $plugins, $config );
 }
-add_action( 'tgmpa_register', 'ca_register_required_plugins' );
+add_action( 'tgmpa_register', 'create_ape_register_required_plugins' );
 
 /**
  * Changes ACF Save-JSON directory
@@ -219,10 +142,10 @@ add_action( 'tgmpa_register', 'ca_register_required_plugins' );
  * @param  String $path Default ACF JSON directory
  * @return String Modified ACF JSON directory path
  */
-function ca_acf_json_save_directory( $path ) {
+function create_ape_acf_json_save_directory( $path ) {
 	return get_stylesheet_directory() . '/acf-json';
 }
-add_filter( 'acf/settings/save_json', 'ca_acf_json_save_directory' );
+add_filter( 'acf/settings/save_json', 'create_ape_acf_json_save_directory' );
 
 /**
  * Changes ACF Load-JSON Directory
@@ -230,10 +153,10 @@ add_filter( 'acf/settings/save_json', 'ca_acf_json_save_directory' );
  * @param Array $paths ACF JSON directory paths
  * @return String Modified ACF JSON directory paths
  */
-function ca_acf_json_load_directory( $paths ) {
+function create_ape_acf_json_load_directory( $paths ) {
 	unset( $paths[0] );
 	$paths[] = get_stylesheet_directory() . '/acf-json';
 
 	return $paths;
 }
-add_filter('acf/settings/load_json', 'ca_acf_json_load_directory');
+add_filter('acf/settings/load_json', 'create_ape_acf_json_load_directory');

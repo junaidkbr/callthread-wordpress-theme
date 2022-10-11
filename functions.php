42e478f112395e4c11719a889923a48b0c97b50c
Junaid Ahmed <junaidkbr@gmail.com>
2022-10-11 22:58:36 +0500

    Removes unused code

--- functions.php
+++ functions.php
@@ -150,11 +150,6 @@ function create_ape_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'create_ape_theme_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
@@ -163,18 +158,6 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 
 /**
  * Changes ACF Save-JSON directory

f6c1975e0d0566347b5599ceb14ff1a3e9c3ef18
Junaid Ahmed <junaidkbr@gmail.com>
2022-10-11 23:10:43 +0500

    Adds ACF custom functions

--- functions.php
+++ functions.php
@@ -73,6 +73,16 @@ function create_ape_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'create_ape_theme_scripts' );
 
 /**
+ * Removes Gutenberg Block Library CSS
+ */
+function create_ape_remove_wp_block_library_css(){
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-blocks-style' );
+ }
+add_action( 'wp_enqueue_scripts', 'create_ape_remove_wp_block_library_css', 100 );
+
+/**
  * Plugin dependency class
  */
 require get_template_directory() . '/inc/class-tgm-plugin-activation.php';
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -163,3 +163,112 @@ if ( ! function_exists( 'wp_body_open' ) ) :
 		do_action( 'wp_body_open' );
 	}
 endif;
+
+/**
+ * Renders ACF Flexible Content a.k.a Blocks
+ *
+ * @param String $template The flexible layout field-group name
+ */
+function ca_render_acf_blocks( $template = 'blocks', $post_id = null ) {
+  if ( ! $post_id ) {
+    $post_id = get_the_ID();
+  }
+
+  if ( get_post_status($post_id) !== 'publish' ) {
+  	return;
+  }
+
+  if ( have_rows( $template, $post_id ) ) {
+    $layout_index = 1;
+
+    while ( have_rows( $template, $post_id ) ) {
+      the_row();
+
+      set_query_var( 'layout_index', $post_id . '--' . $layout_index );
+      $layout = str_replace( '_', '-', get_row_layout() );
+      get_template_part( 'template-parts/blocks/block', $layout );
+
+      $layout_index++;
+    }
+  }
+}
+
+/**
+ *  Print HTML section ID from Composer field
+ */
+function ca_section_id() {
+  $section_id = '';
+
+  if ( $section_id = get_sub_field( 'section_id' ) ) {
+    $section_id = 'id="' . $section_id . '"';
+  }
+
+  echo $section_id;
+}
+
+/**
+ * Custom ACF Field Wrapper
+ *
+ * @param String $name         Name of the ACF field
+ * @param String $before       HTML Markup before the field
+ * @param String $after        HTML Markup after the field
+ * @param Boolean $sub_field   If the field is subfield
+ * @param Boolean $option      If the field is an option
+ */
+function ca_the_field( $name = false, $before = '', $after = '', $sub_field = false, $option = false ) {
+  if ( ! $name ) {
+    return;
+  }
+
+  $output = '';
+
+  if ( ! $option ) {
+    if ( ! $sub_field && get_field( $name ) ) {
+      $output = get_field( $name, false, false );
+    } else if ( $sub_field && get_sub_field( $name ) ) {
+      $output = get_sub_field( $name );
+    }
+  } else {
+    if ( ! $sub_field && get_field( $name, 'option' ) ) {
+      $output = get_field( $name, 'option' );
+    } else if ( $sub_field && get_sub_field( $name ) ) {
+      $output = get_sub_field( $name, 'option' );
+    }
+  }
+
+  if ( ! empty( $output ) ) {
+    echo $before . do_shortcode( $output ) . $after;
+  }
+}
+
+/**
+ * Prints an anchor tag from provided input
+ *
+ * @param Array   $link         Link array
+ * @param String  $css_class    CSS classes
+ * @param String  $target       Target attribute
+ * @param String  $before       HTML Markup before
+ * @param String  $after        HTML Markup after
+ * @param String  $attributes   Custom HTML Attributes
+ */
+function ca_the_link( $link = array(),  $css_classes = '', $target = '', $before = '', $after = '', $attributes = '' ) {
+  if ( ! is_array( $link ) || ! count( $link ) ) {
+    return;
+  }
+
+  $output = '';
+
+  if ( ! empty( $target ) ) {
+    $link['target'] = $target;
+  }
+
+  if ( ! strpos( $attributes, 'aria-label' ) ) {
+    $attributes .= ' aria-label="' . wp_sprintf( __( 'Open for %s', 'fga' ), strip_tags( $link['url'] ) ) . '"';
+  }
+
+  $output .= "<a href=\"{$link['url']}\" class=\"{$css_classes}\" target=\"{$link['target']}\" {$attributes}>{$link['title']}</a>";
+
+  if ( ! empty( $output ) ) {
+    echo $before . $output . $after;
+  }
+}
